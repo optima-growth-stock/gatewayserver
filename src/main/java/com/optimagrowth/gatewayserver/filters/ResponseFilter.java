946eb69b1b92da46d89e46e1796e0aa3619279ed
Toni Vrdoljak <toni.vrdoljak4@gmail.com>
2022-09-25 00:03:19 +0200

    Add sleuth trace id to API gateway response

--- src/main/java/com/optimagrowth/gatewayserver/filters/ResponseFilter.java
+++ src/main/java/com/optimagrowth/gatewayserver/filters/ResponseFilter.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.sleuth.Tracer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
@@ -16,14 +17,14 @@ public class ResponseFilter {
     final Logger logger = LoggerFactory.getLogger(ResponseFilter.class);
 
     @Autowired
-    FilterUtils filterUtils;
+    private Tracer tracer;
 
     @Bean
     public GlobalFilter postGlobalFilter() {
         return (exchange, chain) -> {
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
                 HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
-                String correlationId = filterUtils.getCorrelationId(requestHeaders);
+                String correlationId = tracer.currentSpan().context().traceId();
                 logger.debug("Adding the correlation id to the outbound headers. {}", correlationId);
                 exchange.getResponse().getHeaders().add(FilterUtils.CORRELATION_ID, correlationId);
                 logger.debug("Completing outgoing response for {} request.", exchange.getRequest().getURI());
