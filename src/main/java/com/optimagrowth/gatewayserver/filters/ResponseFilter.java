3f28f5a9234851b464c19a937eba88109b957b12
Toni Vrdoljak <toni.vrdoljak4@gmail.com>
2022-09-25 01:58:14 +0200

    Fix sleuth trace id

--- src/main/java/com/optimagrowth/gatewayserver/filters/ResponseFilter.java
+++ src/main/java/com/optimagrowth/gatewayserver/filters/ResponseFilter.java
@@ -5,11 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
 import org.springframework.cloud.sleuth.Tracer;
+import org.springframework.cloud.sleuth.instrument.web.WebFluxSleuthOperators;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
 
 import reactor.core.publisher.Mono;
+
+import java.util.Objects;
 
 @Configuration
 public class ResponseFilter {
@@ -23,10 +25,13 @@ public class ResponseFilter {
     public GlobalFilter postGlobalFilter() {
         return (exchange, chain) -> {
             return chain.filter(exchange).then(Mono.fromRunnable(() -> {
-                HttpHeaders requestHeaders = exchange.getRequest().getHeaders();
-                String correlationId = tracer.currentSpan().context().traceId();
-                logger.debug("Adding the correlation id to the outbound headers. {}", correlationId);
-                exchange.getResponse().getHeaders().add(FilterUtils.CORRELATION_ID, correlationId);
+                WebFluxSleuthOperators.withSpanInScope(tracer, Objects.requireNonNull(tracer.currentTraceContext()), exchange, () ->
+                        {
+                            String correlationId = tracer.currentTraceContext().context().traceId();
+                            logger.debug("Adding the correlation id to the outbound headers. {}", correlationId);
+                            exchange.getResponse().getHeaders().add(FilterUtils.CORRELATION_ID, correlationId);
+                        }
+                );
                 logger.debug("Completing outgoing response for {} request.", exchange.getRequest().getURI());
             }));
         };
